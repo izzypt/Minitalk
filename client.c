fcf52cc4317e22adf313973d23c731a2655c3df5
Simao <bonvalots@gmail.com>
2023-04-07 12:22:15 +0100

    Sigaction and sending signals

--- client.c
+++ client.c
@@ -2,10 +2,36 @@
 #include <stdio.h>
 #include <signal.h>
 
+int ft_atoi(char *str)
+{
+    int i;
+    int sign;
+    int result;
+
+    sign = 1;
+    i = 0;
+    result = 0;
+
+    if (str[i] == '-')
+    {
+        sign = sign * (-1);
+        str++;
+    }
+
+    while (*str)
+    {
+        result = result * 10 + *str - '0';
+        str++;
+    }
+    return result * sign;
+}
+
 int main(int argc, char **argv)
 {
-    printf("The process id of the server is: %d\n", 526);
-    printf("Client ready to send signals..\n");
-    printf("%s\n", argv[0]);
-    kill(526, 12);
+    printf("The process id of the server is: %d\n", ft_atoi(argv[1]));
+    printf("Client ready to send signals..\nClient pid: %d", getpid());
+    //printf("%s\n", argv[0]);
+    //printf("%s\n", argv[1]);
+    //printf("%s\n", argv[2]);
+    kill(ft_atoi(argv[1]), 12);
 }
--- server.c
+++ server.c
@@ -2,26 +2,31 @@
 #include <stdio.h>
 #include <signal.h>
 
-void    handle_sigusr(int sig)
-{
-    printf("Received signal %d\n", sig);
-
-    printf("Received some signal!");
-}
+void handle_sigusr(int sig, siginfo_t *info, void *ucontext);
 
 int main(int argc, char **argv)
 {
     int pid;
+    struct sigaction	action;
 
     pid = getpid();
 
     printf("The process id of the server is: %d\n", pid);
     printf("Waiting for signals..\n");
 
-    signal(SIGUSR1, handle_sigusr);
-    
+    action.sa_sigaction = handle_sigusr;
+	sigaction(SIGUSR1, &action, 0);
+	sigaction(SIGUSR2, &action, 0);
     while (1)
     {
-        
+        pause();
     }
+    return (0);
+}
+
+void handle_sigusr(int sig, siginfo_t *info, void *ucontext)
+{
+    printf("Received signal %d\n", sig);
+    printf("Received some signal!");
+    //printf("Client pid: %d\n", info->si_pid);
 }
