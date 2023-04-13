f8891b4bb22cb4775dc0b316a72b6e4eaf0c012f
Simão Bonvalot <73948790+izzypt@users.noreply.github.com>
2023-04-13 17:01:38 +0100

    Update server.c

--- server.c
+++ server.c
@@ -1,32 +1,54 @@
-#include <unistd.h>
-#include <stdio.h>
-#include <signal.h>
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   server.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: smagalha <smagalha@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/04/08 20:57:33 by smagalha          #+#    #+#             */
+/*   Updated: 2023/04/08 22:45:36 by smagalha         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
-void handle_sigusr(int sig, siginfo_t *info, void *ucontext);
+#include "minitalk.h"
 
-int main(int argc, char **argv)
+static void	handle_signal(int sig, siginfo_t *info, void *context);
+
+int	main(void)
 {
-    int pid;
-    struct sigaction	action;
+	struct sigaction	action;
 
-    pid = getpid();
-
-    printf("The process id of the server is: %d\n", pid);
-    printf("Waiting for signals..\n");
-
-    action.sa_sigaction = handle_sigusr;
+	ft_putstr_fd("Server PID: ", 1);
+	ft_putnbr_fd(getpid(), 1);
+	ft_putchar_fd('\n', 1);
+	action.sa_sigaction = handle_signal;
+	action.sa_flags = SA_SIGINFO;
 	sigaction(SIGUSR1, &action, 0);
 	sigaction(SIGUSR2, &action, 0);
-    while (1)
-    {
-        pause();
-    }
-    return (0);
+	while (1)
+		pause();
+	return (0);
 }
 
-void handle_sigusr(int sig, siginfo_t *info, void *ucontext)
+static void	handle_signal(int sig, siginfo_t *info, void *context)
 {
-    printf("Received signal %d\n", sig);
-    printf("Received some signal!");
-    //printf("Client pid: %d\n", info->si_pid);
+	static int				bit_count;
+	static unsigned char	c;
+
+	(void)context;
+	c |= (sig == SIGUSR2);
+	if (++bit_count == 8)
+	{
+		bit_count = 0;
+		if (!c)
+		{
+			kill(info->si_pid, SIGUSR2);
+			info->si_pid = 0;
+			return ;
+		}
+		ft_putchar_fd(c, 1);
+		c = 0;
+	}
+	else
+		c <<= 1;
 }
